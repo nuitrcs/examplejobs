fe5e2a7e4930a6646e3ddd407dec129ae71b9a53
Christina Maimone <christina.maimone@northwestern.edu>
2017-06-26 21:39:50 -0500

    Added basic R example

---
+++ r/commandline_arguments/commandlineargs.R
@@ -0,0 +1,22 @@
+library(dplyr) ## an example, not used below; install packages ahead of time in your account
+
+## save command line arguments
+args <- commandArgs(trailingOnly = TRUE)
+n <- as.integer(args[2])+1
+
+## write file, using arguments for filename
+fileConn<-file(paste0(args[1],".txt"))
+writeLines(c(paste(args[1], n)), fileConn)
+close(fileConn)
+
+cat("Test output") # this will direct to stdout, which will go to an output file generated as part of the job submission
+
+
+## make and save a plot as pdf
+pdf() ## with no file specified, will default to Rplots.pdf in working directory
+plot(1:5,1:5)
+dev.off()
+
+## because the above runs quickly, sleep a bit so you can see the process running if you want;
+## not necessary, but useful for showing use of job management commands
+# Sys.sleep(60)
---
+++ r/commandline_arguments/myjob.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#MSUB -A <allocationID>
+#MSUB -q <queueName>
+#MSUB -l walltime=<hh:mm:ss>
+#MSUB -M <emailAddress>
+#MSUB -j oe
+#MSUB -N <jobName>
+#MSUB -l nodes=1:ppn=<numberOfCores>
+
+# Leave a blank line, like above, before you start your other commands
+
+# with #MSUB, a # doesn't indicate a comment;
+# it's part of the MSUB specification (and first line).
+# In the rest of the script, # starts a comment
+
+# load module with version you want
+module load R/3.3.2
+
+# Set your working directory 
+# This sets it to the directory you're submitting from -- change as appropriate
+cd $PBS_O_WORKDIR
+
+Rscript commandlineargs.R myoutputfile 14
