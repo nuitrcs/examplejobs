44c110e87026f12a6f9d51f4fa5887fde84d29de
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2021-02-24 19:07:24 -0600

    Adding examples of running QE NWCHEM and NAMD, highlighting the latest modules available on QUEST via spack. You can run these MPI programs using srun now if you would like

---
+++ namd/namd_submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=a9009  ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --partition=master  ### PARTITION (buyin, short, normal, etc)
+#SBATCH --nodes=1 ## how many computers do you need
+#SBATCH --ntasks-per-node=14 ## how many cpus or processors do you need on each computer
+#SBATCH --cpus-per-task=1 ## how many cpus or processors do you need on each computer
+#SBATCH --time=00:20:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --mem=0 ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --job-name=sample_job  ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --output=outlog_mpi_smp_2_node ## standard out and standard error goes to this file
+#SBATCH --constraint="[quest6]"
+
+module purge all
+module load namd/2.14-openmpi-4.0.5-intel-19.0.5.281 
+
+srun --mpi=pmix_v3 -n ${SLURM_NNODES} namd2 ++ppn $((${SLURM_NTASKS_PER_NODE}-1)) alanin.conf
---
+++ quantum-espresso/quantum_espresso_submit.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --account=a9009  ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --partition=master  ### PARTITION (buyin, short, normal, etc)
+#SBATCH --nodes=2 ## how many computers do you need
+#SBATCH --ntasks-per-node=4 ## how many cpus or processors do you need on each computer
+#SBATCH --time=00:20:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --mem-per-cpu=3G ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --job-name=sample_job  ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --output=outlog_qe_6.7_intel ## standard out and standard error goes to this file
+#SBATCH --constraint="[quest5|quest6|quest8|quest9|quest10]"
+
+module purge all
+module load quantum-espresso/6.7-openmpi-4.0.5-intel-19.0.5.281
+
+mpirun -np $SLURM_NTASKS pw.x < ./qe.in > ./qe.out
+#srun --mpi=pmix_v3 -n ${SLURM_NTASKS} pw.x < ./qe.in > ./qe.out
