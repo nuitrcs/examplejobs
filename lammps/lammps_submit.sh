375c8d4aa93c7ede2248b99cd6a9e521dbd901a8
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2021-03-03 14:48:14 -0600

    Adding lammps example

---
+++ lammps/lammps_submit.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --account=a9009  ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --partition=master  ### PARTITION (buyin, short, normal, etc)
+#SBATCH --nodes=2 ## how many computers do you need
+#SBATCH --ntasks-per-node=4 ## how many cpus or processors do you need on each computer
+#SBATCH --time=00:20:00 ## how long does this need to run (remember different partitions have restrictions on this param)
+#SBATCH --mem-per-cpu=3G ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
+#SBATCH --job-name=lammps_openmpi  ## When you run squeue -u NETID this is how you can identify the job
+#SBATCH --output=outlog_lammps_openmpi_intel ## standard out and standard error goes to this file
+#SBATCH --constraint="[quest5|quest6|quest8|quest9|quest10]"
+
+module purge ALL
+module load lammps/20200303-openmpi-4.0.5-intel-19.0.5.281
+
+mpirun -np ${SLURM_NTASKS} lmp -in in.lj
+#srun --mpi=pmix_v3 lmp -in in.lj
+
