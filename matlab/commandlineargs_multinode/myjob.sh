1ed2f0a71b61e672ed8fb724caae7dbe2156771f
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2021-05-18 11:09:57 -0500

    Alright this is a healthy update to the example jobs

--- matlab/commandlineargs_multinode/myjob.sh
+++ matlab/commandlineargs_multinode/myjob.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-module load matlab/r2016a
+module load matlab/r2020b
 # script1 is your .m file without the extension; mylogfile.txt will be created
 matlab -nosplash -nodesktop -singleCompThread -r "numiter=36;script1" > mylogfile.txt &
 exit
