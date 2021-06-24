e452cc9a734698952d2d5af46cd2b2a40c808ae6
scottcoughlin2014 <scottcoughlin2014@u.northwestern.edu>
2021-06-24 07:51:13 -0500

    Updating constraints to reflect the new quest7 (and the fact that quest 5 and quest6 are going away

--- abinit/abinit_submit.sh
+++ abinit/abinit_submit.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
-#SBATCH --account=a9009  ## YOUR ACCOUNT pXXXX or bXXXX
-#SBATCH --partition=master  ### PARTITION (buyin, short, normal, etc)
+#SBATCH --account=pXXXX  ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --partition=short  ### PARTITION (buyin, short, normal, etc)
 #SBATCH --nodes=2 ## how many computers do you need
 #SBATCH --ntasks-per-node=4 ## how many cpus or processors do you need on each computer
 #SBATCH --time=00:20:00 ## how long does this need to run (remember different partitions have restrictions on this param)
 #SBATCH --mem-per-cpu=3G ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=sample_job  ## When you run squeue -u NETID this is how you can identify the job
 #SBATCH --output=outlog_abinit_8_10_3_intel ## standard out and standard error goes to this file
-#SBATCH --constraint="[quest5|quest6|quest8|quest9|quest10]"
+#SBATCH --constraint="[quest7|quest8|quest9|quest10]"
 
 module purge all
 module load abinit/8.10.3-openmpi-4.0.5-intel-19.0.5.281
--- namd/namd_submit.sh
+++ namd/namd_submit.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --account=a9009  ## YOUR ACCOUNT pXXXX or bXXXX
-#SBATCH --partition=master  ### PARTITION (buyin, short, normal, etc)
+#SBATCH --account=pXXXX  ## YOUR ACCOUNT pXXXX or bXXXX
+#SBATCH --partition=short  ### PARTITION (buyin, short, normal, etc)
 #SBATCH --nodes=1 ## how many computers do you need
 #SBATCH --ntasks-per-node=14 ## how many cpus or processors do you need on each computer
 #SBATCH --cpus-per-task=1 ## how many cpus or processors do you need on each computer
@@ -8,7 +8,7 @@
 #SBATCH --mem=0 ## how much RAM do you need per CPU (this effects your FairShare score so be careful to not ask for more than you need))
 #SBATCH --job-name=sample_job  ## When you run squeue -u NETID this is how you can identify the job
 #SBATCH --output=outlog_mpi_smp_2_node ## standard out and standard error goes to this file
-#SBATCH --constraint="[quest5|quest6|quest8|quest9|quest10]"
+#SBATCH --constraint="[quest7|quest8|quest9|quest10]"
 
 module purge all
 module load namd/2.14-openmpi-4.0.5-intel-19.0.5.281 
