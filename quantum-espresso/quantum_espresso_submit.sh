2848060c96e5d58d4786e0c1535aac32dcc1c7b5
Scott Coughlin <scottcoughlin2014@u.northwestern.edu>
2021-11-08 15:39:01 -0600

    Update quantum_espresso_submit.sh

--- quantum-espresso/quantum_espresso_submit.sh
+++ quantum-espresso/quantum_espresso_submit.sh
@@ -11,7 +11,6 @@
 
 module purge all
 module load quantum-espresso/6.7-openmpi-4.0.5-intel-19.0.5.281
-export SLURM_MPI_TYPE=pmix_v3
 
 mpirun -np $SLURM_NTASKS pw.x < ./qe.in > ./qe.out
 #srun -n ${SLURM_NTASKS} pw.x < ./qe.in > ./qe.out
