82b6ccc272d90f91ef60d85818e9f9da17966542
Scott Coughlin <scottcoughlin2014@u.northwestern.edu>
2022-11-11 11:22:13 -0600

    Update dependent_job_wrapper.sh
    Improving the dependent job wrapper script

--- dependent_job/dependent_job_wrapper.sh
+++ dependent_job/dependent_job_wrapper.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-jid0=($(sbatch --time=00:10:00 --account=w10001 --partition=w10001 --nodes=1 --ntasks-per-node=1 --mem=8G --job-name=example --output=job_%A.out example_submit.sh))
+jid0=$(sbatch --parsable --time=00:10:00 --account=a9009 --partition=a9009 --nodes=1 --ntasks-per-node=1 --mem=8G --job-name=example --output=job_%A.out example_submit.sh)
 
-echo "jid0 ${jid0[-1]}" >> slurm_ids
+echo "jid0 ${jid0}" >> slurm_ids
 
-jid1=($(sbatch --dependency=afterok:${jid0[-1]} --time=00:10:00 --account=w10001 --partition=w10001 --nodes=1 --ntasks-per-node=1 --mem=8G --job-name=example --output=job_%A.out --export=DEPENDENTJOB=${jid0[-1]} example_submit.sh))
+jid1=$(sbatch --parsable --dependency=afterok:${jid0} --time=00:10:00 --account=a9009 --partition=a9009 --nodes=1 --ntasks-per-node=1 --mem=8G --job-name=example --output=job_%A.out --export=DEPENDENTJOB=${jid0} example_submit.sh)
 
-echo "jid1 ${jid1[-1]}" >> slurm_ids
+echo "jid1 ${jid1}" >> slurm_ids
 
-jid2=($(sbatch --dependency=afterok:${jid1[-1]} --time=00:10:00 --account=w10001 --partition=w10001 --nodes=1 --ntasks-per-node=1 --mem=8G --job-name=example --output=job_%A.out --export=DEPENDENTJOB=${jid1[-1]} example_submit.sh))
+jid2=$(sbatch --parsable --dependency=afterok:${jid1} --time=00:10:00 --account=a9009 --partition=a9009 --nodes=1 --ntasks-per-node=1 --mem=8G --job-name=example --output=job_%A.out --export=DEPENDENTJOB=${jid1} example_submit.sh)
 
-echo "jid2 ${jid2[-1]}" >> slurm_ids
+echo "jid2 ${jid2}" >> slurm_ids
