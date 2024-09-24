d09f2890dda18a5608868b7b9acc6f3b03b6bad3
Scott Coughlin <scottcoughlin2014@u.northwestern.edu>
2024-09-24 14:35:25 -0500

    Update myjob.sh

--- matlab/basic_multi_node/myjob.sh
+++ matlab/basic_multi_node/myjob.sh
@@ -1,6 +1,1 @@
-#!/bin/bash
 
-module load matlab/r2020b
-# script1 is your .m file without the extension; mylogfile.txt will be created
-matlab -nosplash -nodesktop -singleCompThread -r script1 > mylogfile.txt &
-exit
