729cbbff2827f7a646122997412018db565c409e
hscarter <haley_carter@brown.edu>
2025-02-14 09:44:20 -0600

    added bwa example from fall 2024 workshop

--- bwa/bwa.sh
+++ bwa/bwa.sh
@@ -1,31 +1,27 @@
 #!/bin/bash
 
-#SBATCH --account=b1042
-#SBATCH --partition=genomics
-#SBATCH --job-name=bwa
+#SBATCH --account=e32559
+#SBATCH --partition=normal
+#SBATCH --job-name=bwa_align
 #SBATCH --output=bwa.out
 #SBATCH --error=bwa.err
-#SBATCH --time=30:00:00
+#SBATCH --time=10:00:00
 #SBATCH --mem=10G       ### edit this line to reserve more/less memory
 #SBATCH --ntasks=8      ### edit this line to reserve different no. of cores
 #SBATCH --nodes=1
 
 module load bwa/0.7.17
 
-# first line:
-# create the genome index for alignment, in this case GRCh38 version of human genome with the prefix (-p) GRCh38_idx
-# bash loop:
-# in this directory there is a file called filenames.txt, for every unique file prefix in that file, the bwa mem command within the loop will be run
-# -t specifies the threads to use, followed by the index created in the initial line, then fastqs are specified to run bwa-mem in pe mode and the output is redirected to the output SAM files before closing the loop.
-# Anthony Pulvino; 10/24/2022
+export REF_GENOME=/projects/e32559/example_data/oenotheraHarringtonii.softmasked.fasta
+export SAMPLE_DIR=/projects/e32559/example_data
 
-export genome_fa_dir=/projects/genomicsshare/GCC_scripts/bwa
-export example_files=/projects/genomicsshare/GCC_scripts/bwa/sequence_read
 
-bwa index $genome_fa_dir/GRCh38_HumanGenome.fa -p GRCh38_idx
+# remember to index genome before aligning
+# if you're aligning many samples indexing should be done previously
+#bwa index $REF_GENOME
 
-cat $example_files/filenames.txt | while read line; do \
-        file=$line;
+# align reads to soft masked genome
+bwa mem -t $SLURM_NPROCS $REF_GENOME \
+  $SAMPLE_DIR/TRIN_5690_11_S31_L001_R1_001.fastq.gz \
+  $SAMPLE_DIR/TRIN_5690_11_S31_L001_R2_001.fastq.gz > TRIN_5690_11_S31_L001_aln-pe.sam
 
-        bwa mem -t 8 GRCh38_idx $example_files/${file}_1.filt.fastq $example_files/${file}_2.filt.fastq > ${file}_aln_pe.sam
-done
